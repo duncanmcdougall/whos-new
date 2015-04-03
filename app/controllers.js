6e5421055fa91e619905bc556a6cf08dec7e6bf7
Duncan McDOugall <duncanjmcdougall@gmail.com>
2015-04-03 11:42:04 +0100

    part way through. level step logic

--- app/controllers.js
+++ app/controllers.js
@@ -49,32 +49,54 @@ app.controller('GameCtrl', ['$scope', '$interval', function ($scope, $interval) 
     $scope.step = 1;
     $scope.time = 0;
     $scope.started = false;
-    
     var timerInterval;
+    $scope.countdown = 3;
+
+    $scope.chance = function(item) {
+        if(item.isNew) {   
+            $('.header__progress').addClass('animated pulse');
+            $scope.step++;
+            console.log("CORRECT");
+        }else{
+            console.log("WRONG");   
+        }
+    };
     
-    var i;
-    for(i = 0; i < 15; i++)
-    {
-        $scope.items.push({
-            color: colors[_.random(colors.length-1)],
-            icon: types[i]
-        });
-    }
+    $scope.reset = function() {
+        types = shuffle(types);  
+        $scope.step = 0;
+        $scope.started = false;
+        $scope.time = 0;
+        $interval.cancel(timerInterval);
+    };
     
-    $scope.chance = function() {
-        $('.header__progress').addClass('animated pulse');
-        $scope.step++;
+    
+    var readyInterval;
+    
+    $scope.getReady = function() {
+        $scope.items = generatePack(0);
+        $scope.started = true;
+        readyInterval = $interval(function() {
+             $scope.countdown--;
+            if( $scope.countdown == 0) {
+                $scope.start();
+                $interval.cancel(readyInterval);
+            }
+        }, 1000);
     };
     
     $scope.start = function() {
-        $scope.started = true;
-        $scope.time = 0;
-        
-        $interval.cancel(timerInterval);
+        $scope.step = 1;
+        $scope.items = generatePack(1);
+
         timerInterval = $interval(function() {
             $scope.time += 0.1;
         }, 100);
     };
+    
+    $
+    
+    $scope.reset();
 }]);
 
 app.filter('numberFixedLen', function () {
@@ -87,3 +109,53 @@ app.filter('numberFixedLen', function () {
         return num.toFixed(len);
     };
 });
+
+function generatePack(step) {
+    var i = 0;
+    
+    var items = [];
+    
+    falsesToShow = 15 - step;
+    
+    for(i; i < falsesToShow; i++) 
+    {
+        items.push({
+            color: colors[_.random(colors.length-1)],
+            icon: types[0],
+            isNew: false
+        });
+    }
+    
+    var t = 0;
+    for(t; t < step; t++) {
+        items.push({
+            color: colors[_.random(colors.length-1)],
+            icon: types[step],
+            isNew: true
+        });
+    }
+    
+    items = shuffle(items);
+    
+    return items;
+
+};
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
